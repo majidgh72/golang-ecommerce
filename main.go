4223bd7a4f708a2e92b208db45906656ef46d1f5
Majid Ghafoorzade <m_ghafoorzadeh@namava.local>
2023-08-02 16:02:23 +0330

    feat: add .env config

--- main.go
+++ main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"log"
+	"os"
 	"rismoon/pkg/modules"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
 
+	// load env variables
+	envError := godotenv.Load()
+	if envError != nil {
+		log.Fatal("Unable to read .env file")
+	}
+
 	// create fiber app instance
 	app := fiber.New()
 
 	// conbect to database
-	dsn := "root:@tcp(127.0.0.1:3306)/go?charset=utf8mb4&parseTime=True&loc=Local"
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := dbUser + ":" + dbPass + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
