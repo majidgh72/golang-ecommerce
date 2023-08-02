f6474e38c42be7928b940754923a6d5f4ef1a461
Majid Ghafoorzade <m_ghafoorzadeh@namava.local>
2023-08-02 16:06:19 +0330

    refactor database config

--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"log"
-	"os"
+	"rismoon/pkg/db"
 	"rismoon/pkg/modules"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
 func main() {
@@ -23,21 +21,10 @@ func main() {
 	app := fiber.New()
 
 	// conbect to database
-	dbUser := os.Getenv("DB_USER")
-	dbPass := os.Getenv("DB_PASS")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
-
-	dsn := dbUser + ":" + dbPass + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	if err != nil {
-		panic("Databaseconnection failed")
-	}
+	Database := db.Register()
 
 	// register modules
-	modules.Register(app, db)
+	modules.Register(app, Database)
 
 	// listen app
 	app.Listen(":3000")
---
+++ pkg/db/main.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func Register() *gorm.DB {
+
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := dbUser + ":" + dbPass + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Database connection failed")
+	}
+
+	return db
+}
